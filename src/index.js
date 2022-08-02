9f1831e825f9c9633c48b78830f8e8de6f79e353
BiaChamsim <biachamsim@gmail.com>
2022-08-02 12:57:12 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import express, {json} from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+const app = express();
+
+dotenv.config();
+app.use(cors());
+app.use(json());
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log('servidor funfando!!')
+});
