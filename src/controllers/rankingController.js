8b0bebb690f42a107dce0bf82ea4ed1a70120c15
BiaChamsim <biachamsim@gmail.com>
2022-08-07 19:16:48 -0300

    rota ranking finalizada

--- src/controllers/rankingController.js
+++ src/controllers/rankingController.js
@@ -4,17 +4,21 @@ export async function getRanking(req,res){
     try{
 
         const ranking = await db.query(
-            `SELECT users.id, users.name 
-            COUNT ("url") AS "linksCount"
+            `SELECT users.id, users.name, 
+            COUNT ("url") AS "linksCount",
             SUM("visitCount") AS "visitCount"
             FROM users 
-            JOIN urls
+            LEFT JOIN urls
             ON urls."userId" = users.id 
             GROUP BY users.id
             ORDER BY "visitCount" DESC LIMIT 10`
         );
 
-        res.send(ranking.rows).status(200)
+        const topTenUsers = ranking.rows.map(userData => userData.visitCount ? userData : {...userData, visitCount: "0"})
+
+        const ordenedTopten = [...topTenUsers].sort((a, b) => b.visitCount - a.visitCount)
+
+        res.send(ordenedTopten).status(200)
 
     }catch(err){
         console.log(err)
--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,16 +1,35 @@
 import db from "../db.js";
 
+function formatDataUrl({url, visitCount, urlId, shortUrl}){
+    return {id: urlId, shortUrl, url, visitCount};
+}
+
 export async function getUsers(req, res){
     try{
         const {userId} = res.locals;
 
         const userData = await db.query(
-            `SELECT * FROM users 
-            WHERE id = $1`,
-            [id]
+            `SELECT users.id, users.name, urls.url, urls.id AS "urlId", 
+            urls."shortUrl", urls."visitCount" 
+            FROM users 
+            LEFT JOIN urls
+            ON "userId" = users.id
+            WHERE users.id = $1
+            GROUP BY users.id, urls.url, urls.id`,
+            [userId]
         )
 
+        let totalVisits = 0;
+        userData.rows.forEach(userUrlData => totalVisits += parseInt(userUrlData.visitCount))
 
+        const userStatistic = {
+            id: userData.rows[0].id,
+            name: userData.rows[0].name,
+            visitCount: totalVisits ? totalVisits : "0",
+            shortenedUrls: userData.rows[0].urlId ? userData.rows.map(formatDataUrl) : [] 
+        }
+
+        res.send(userStatistic).status(200);
 
     }catch(err){
         console.log(err)
