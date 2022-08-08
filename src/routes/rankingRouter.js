fcb0503c511bfde91f2385c2706e94c266a1af68
BiaChamsim <biachamsim@gmail.com>
2022-08-08 17:07:57 -0300

    ajuste de autenticação

--- src/routes/rankingRouter.js
+++ src/routes/rankingRouter.js
@@ -1,13 +1,12 @@
 import { Router } from "express";
 
 import { getRanking } from "../controllers/rankingController.js";
-import validateToken from "../middlewares/validateToken.js"
 
 
 const router = Router();
 
 
-router.get("/ranking", validateToken, getRanking);
+router.get("/ranking", getRanking);
 
 
 
